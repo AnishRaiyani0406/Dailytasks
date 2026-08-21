71fd46b056ac1021ca233eb63d7b2ade72d7726f
anishraiyani <anishraiyani@gmail.com>
2026-08-21 19:30:11 +0530

    Initial commit

---
+++ react-router/src/App.jsx
@@ -0,0 +1,22 @@
+import { Route,Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+
+function App(){
+
+  return (
+    <div>
+       
+       <Routes>
+
+        <Route path = "/" element = {<Home />}  />
+        <Route path = "/about"  element= {<About /> } />
+       </Routes>
+       
+
+    </div>
+  )
+}
+
+export default App;
